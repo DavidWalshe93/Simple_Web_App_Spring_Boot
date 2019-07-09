e01fb9e4639208fa2c861945624d7719818b36b5
david.walshe93@gmail.com <b$P$JpQFY9cg>
2019-07-09 23:28:50 +0100

    - Initialised spring boot project - Added two Pojos, Author and Book - Added Entity, Id, ManyToMany and Generated Value @Annotations - Turned on H2 web viewer

---
+++ src/main/java/learning/spring/spring5_web_app/Spring5WebAppApplication.java
@@ -0,0 +1,13 @@
+package learning.spring.spring5_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/learning/spring/spring5_web_app/model/Book.java
@@ -0,0 +1,82 @@
+package learning.spring.spring5_web_app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Developer: David Walshe
+ * Date: 09/07/2019
+ */
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    @ManyToMany
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
