f180604c6142b69e229f0c9c1946ba6f833b1ef7
david.walshe93@gmail.com <b$P$JpQFY9cg>
2019-07-10 00:21:46 +0100

    - Added data initialisation, bootstrap functionality, for H2 database.

--- src/main/java/learning/spring/spring5_web_app/bootstrap/DevBootstrap.java
+++ src/main/java/learning/spring/spring5_web_app/bootstrap/DevBootstrap.java
@@ -1,8 +1,52 @@
 package learning.spring.spring5_web_app.bootstrap;
 
+import learning.spring.spring5_web_app.model.Author;
+import learning.spring.spring5_web_app.model.Book;
+import learning.spring.spring5_web_app.repositories.AuthorRepository;
+import learning.spring.spring5_web_app.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
 /**
  * Developer: David Walshe
  * Date: 10/07/2019
  */
-public class DevBootstrap {
+// Spring framework component
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    // Autowired by spring framework
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    // Code will run when context is up and running on start up.
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    // Load sample data into H2
+    private void initData() {
+        Author hermin = new Author("Herman", "Melville");
+        Book mobyDick = new Book("Moby Dick", "1234", "Harper Collins");
+        hermin.getBooks().add(mobyDick);
+        mobyDick.getAuthors().add(hermin);
+
+        authorRepository.save(hermin);
+        bookRepository.save(mobyDick);
+
+        Author tolkien = new Author("John", "Tolkien");
+        Book lordOfTheRings = new Book("Lord of the Rings", "4321", "Allen & Unwin");
+        tolkien.getBooks().add(lordOfTheRings);
+        lordOfTheRings.getAuthors().add(tolkien);
+
+        authorRepository.save(tolkien);
+        bookRepository.save(lordOfTheRings);
+    }
 }
