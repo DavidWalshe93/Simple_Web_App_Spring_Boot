155a0fcd26e026ef5dd4f98fb1c2c289b7d6e956
david.walshe93@gmail.com <b$P$JpQFY9cg>
2019-07-10 21:24:22 +0100

    - Created Publisher pojo

---
+++ src/main/java/learning/spring/spring5_web_app/model/Publisher.java
@@ -0,0 +1,53 @@
+package learning.spring.spring5_web_app.model;
+
+import javax.persistence.Entity;
+import java.util.Objects;
+
+/**
+ * Developer: David Walshe
+ * Date: 10/07/2019
+ */
+@Entity
+public class Publisher {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Publisher publisher = (Publisher) o;
+        return Objects.equals(name, publisher.name) &&
+                Objects.equals(address, publisher.address);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
